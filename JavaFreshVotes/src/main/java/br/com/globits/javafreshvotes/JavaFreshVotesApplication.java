14b2a2ddbdf1196888d0881739afedff993d5f8a
Leonardo <Leonardo@14r-3450>
2018-11-18 12:43:19 -0200

    Initian check-in of the JavaFreshVotes code

---
+++ JavaFreshVotes/src/main/java/br/com/globits/javafreshvotes/JavaFreshVotesApplication.java
@@ -0,0 +1,12 @@
+package br.com.globits.javafreshvotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFreshVotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFreshVotesApplication.class, args);
+	}
+}
